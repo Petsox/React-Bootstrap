131b42b670ba8f100965f2d3f528f71cddf4bd6d
Ᵽєҭȿסּꭖ Křížek <92917981+Petsox@users.noreply.github.com>
2023-02-20 19:49:52 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+import { Button, Card, Form } from 'react-bootstrap';
+
+
+function LoginForm() {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>Login</Card.Title>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+}
